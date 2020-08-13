addad22b062b9b57d1eeb0f86b24f865dad858cf
ondrej.machacek <ondrej.machacek@jumpshot.com>
2020-08-13 15:31:25 +0200

    Spark msaccess reader prototype

---
+++ src/main/scala/com/tmobile/sit/common/readers/MSAccessSparkReader.scala
@@ -0,0 +1,27 @@
+package com.tmobile.sit.common.readers
+
+import org.apache.spark.sql.jdbc.{JdbcDialect, JdbcDialects}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+
+case object MSAccessJdbcDialect extends JdbcDialect {
+  override def canHandle(url: String): Boolean = url.startsWith("jdbc:ucanaccess")
+  override def quoteIdentifier(colName: String): String = s"[$colName]"
+}
+
+class MSAccessSparkReader(path: String, tableName: String)(implicit sparkSession: SparkSession) extends Reader {
+  override def read(): DataFrame = {
+    JdbcDialects.registerDialect(MSAccessJdbcDialect)
+
+    sparkSession.read
+      .format("jdbc")
+      .option("url", s"jdbc:ucanaccess://${path};memory=false")
+      .option("dbtable", tableName)
+      .load()
+  }
+}
+
+object MSAccessSparkReader {
+  def apply(path: String, tableName: String)(implicit sparkSession: SparkSession): MSAccessSparkReader
+  = new MSAccessSparkReader(path, tableName)(sparkSession)
+}
